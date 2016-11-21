087113e8748f51bbab01ee24e5f810db94941398
Alexey Fetisov <alfet123@gmail.com>
2016-11-21 21:01:29 +0500

    Отключение ненужных модулей

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,6 @@ const mqpacker = require('css-mqpacker');
 const minify = require('gulp-csso');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
 const webpack = require('gulp-webpack');
 
 gulp.task('style', function () {
