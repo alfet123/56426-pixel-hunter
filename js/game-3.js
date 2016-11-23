e1aa018806f6c55b06ac94ad8a905760b4d7210f
Alexey Fetisov <alfet123@gmail.com>
2016-11-24 01:07:43 +0500

    Отделение данных от верстки

--- js/game-3.js
+++ js/game-3.js
@@ -2,34 +2,59 @@
 import {getElementFromTemplate, select} from './lib';
 import {statsElement} from './stats';
 
-let game3Template = `<header class="header">
+const game3 = {
+  header: {
+    back: 'Back',
+    timer: 'NN',
+    live: 'Life'
+  },
+  task: 'Найдите рисунок среди изображений',
+  content: {
+    option1: {
+      image: 'http://placehold.it/304x455',
+      alt: 'Option 1'
+    },
+    option2: {
+      image: 'http://placehold.it/304x455',
+      alt: 'Option 2'
+    },
+    option3: {
+      image: 'http://placehold.it/304x455',
+      alt: 'Option 3'
+    }
+  }
+};
+
+const header = `<header class="header">
   <div class="header__back">
     <span class="back">
-      <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
+      <img src="img/arrow_left.svg" width="45" height="45" alt="${game3.header.back}">
       <img src="img/logo_small.png" width="101" height="44">
     </span>
   </div>
-  <h1 class="game__timer">NN</h1>
+  <h1 class="game__timer">${game3.header.timer}</h1>
   <div class="game__lives">
-    <img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
-    <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
-    <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+    <img src="img/heart__empty.svg" class="game__heart" alt="${game3.header.live}" width="32" height="32">
+    <img src="img/heart__full.svg" class="game__heart" alt="${game3.header.live}" width="32" height="32">
+    <img src="img/heart__full.svg" class="game__heart" alt="${game3.header.live}" width="32" height="32">
   </div>
-</header>
-<div class="game">
-  <p class="game__task">Найдите рисунок среди изображений</p>
-  <form class="game__content  game__content--triple">
+</header>`;
+
+const task = `<p class="game__task">${game3.task}</p>`;
+
+const content = `<form class="game__content  game__content--triple">
     <div class="game__option">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+      <img src="${game3.content.option1.image}" alt="${game3.content.option1.alt}" width="304" height="455">
     </div>
     <div class="game__option  game__option--selected">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+      <img src="${game3.content.option2.image}" alt="${game3.content.option2.alt}" width="304" height="455">
     </div>
     <div class="game__option">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+      <img src="${game3.content.option3.image}" alt="${game3.content.option3.alt}" width="304" height="455">
     </div>
-  </form>
-  <div class="stats">
+  </form>`;
+
+const stats = `<div class="stats">
     <ul class="stats">
       <li class="stats__result stats__result--wrong"></li>
       <li class="stats__result stats__result--slow"></li>
@@ -42,12 +67,18 @@ let game3Template = `<header class="header">
       <li class="stats__result stats__result--fast"></li>
       <li class="stats__result stats__result--unknown"></li>
     </ul>
-  </div>
+  </div>`;
+
+const game3Template = `${header}
+<div class="game">
+  ${task}
+  ${content}
+  ${stats}
 </div>`;
 
 const game3Element = getElementFromTemplate(game3Template);
 
-let game3Answers = game3Element.querySelectorAll('.game__option');
+const game3Answers = game3Element.querySelectorAll('.game__option');
 
 for (let i = 0; i < game3Answers.length; ++i) {
   game3Answers[i].onclick = () => select(statsElement);
--- js/stats.js
+++ js/stats.js
@@ -1,19 +1,62 @@
 
 import {getElementFromTemplate} from './lib';
 
-let statsTemplate = `<header class="header">
+const stats = {
+  title: 'Победа!',
+  result1: {
+    number: '1.',
+    points: '×&nbsp;100',
+    total: '900',
+    extra1: {
+      name: 'Бонус за скорость:',
+      value: '1&nbsp;',
+      points: '×&nbsp;50',
+      total: '50'
+    },
+    extra2: {
+      name: 'Бонус за жизни:',
+      value: '2&nbsp;',
+      points: '×&nbsp;50',
+      total: '100'
+    },
+    extra3: {
+      name: 'Штраф за медлительность:',
+      value: '2&nbsp;',
+      points: '×&nbsp;50',
+      total: '-100'
+    },
+    final: '950'
+  },
+  result2: {
+    number: '2.',
+    final: 'fail'
+  },
+  result3: {
+    number: '3.',
+    points: '×&nbsp;100',
+    total: '900',
+    extra1: {
+      name: 'Бонус за жизни:',
+      value: '2&nbsp;',
+      points: '×&nbsp;50',
+      total: '100'
+    },
+    final: '1000'
+  }
+};
+
+const header = `<header class="header">
   <div class="header__back">
     <span class="back">
       <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
       <img src="img/logo_small.png" width="101" height="44">
     </span>
   </div>
-</header>
-<div class="result">
-  <h1>Победа!</h1>
-  <table class="result__table">
+</header>`;
+
+const result1 = `<table class="result__table">
     <tr>
-      <td class="result__number">1.</td>
+      <td class="result__number">${stats.result1.number}</td>
       <td colspan="2">
         <ul class="stats">
           <li class="stats__result stats__result--wrong"></li>
@@ -28,37 +71,38 @@ let statsTemplate = `<header class="header">
           <li class="stats__result stats__result--unknown"></li>
         </ul>
       </td>
-      <td class="result__points">×&nbsp;100</td>
-      <td class="result__total">900</td>
+      <td class="result__points">${stats.result1.points}</td>
+      <td class="result__total">${stats.result1.total}</td>
     </tr>
     <tr>
       <td></td>
-      <td class="result__extra">Бонус за скорость:</td>
-      <td class="result__extra">1&nbsp;<span class="stats__result stats__result--fast"></span></td>
-      <td class="result__points">×&nbsp;50</td>
-      <td class="result__total">50</td>
+      <td class="result__extra">${stats.result1.extra1.name}</td>
+      <td class="result__extra">${stats.result1.extra1.value}<span class="stats__result stats__result--fast"></span></td>
+      <td class="result__points">${stats.result1.extra1.points}</td>
+      <td class="result__total">${stats.result1.extra1.total}</td>
     </tr>
     <tr>
       <td></td>
-      <td class="result__extra">Бонус за жизни:</td>
-      <td class="result__extra">2&nbsp;<span class="stats__result stats__result--heart"></span></td>
-      <td class="result__points">×&nbsp;50</td>
-      <td class="result__total">100</td>
+      <td class="result__extra">${stats.result1.extra2.name}</td>
+      <td class="result__extra">${stats.result1.extra2.value}<span class="stats__result stats__result--heart"></span></td>
+      <td class="result__points">${stats.result1.extra2.points}</td>
+      <td class="result__total">${stats.result1.extra2.total}</td>
     </tr>
     <tr>
       <td></td>
-      <td class="result__extra">Штраф за медлительность:</td>
-      <td class="result__extra">2&nbsp;<span class="stats__result stats__result--slow"></span></td>
-      <td class="result__points">×&nbsp;50</td>
-      <td class="result__total">-100</td>
+      <td class="result__extra">${stats.result1.extra3.name}</td>
+      <td class="result__extra">${stats.result1.extra3.value}<span class="stats__result stats__result--slow"></span></td>
+      <td class="result__points">${stats.result1.extra3.points}</td>
+      <td class="result__total">${stats.result1.extra3.total}</td>
     </tr>
     <tr>
-      <td colspan="5" class="result__total  result__total--final">950</td>
+      <td colspan="5" class="result__total  result__total--final">${stats.result1.final}</td>
     </tr>
-  </table>
-  <table class="result__table">
+  </table>`;
+
+const result2 = `<table class="result__table">
     <tr>
-      <td class="result__number">2.</td>
+      <td class="result__number">${stats.result2.number}</td>
       <td>
         <ul class="stats">
           <li class="stats__result stats__result--wrong"></li>
@@ -74,12 +118,13 @@ let statsTemplate = `<header class="header">
         </ul>
       </td>
       <td class="result__total"></td>
-      <td class="result__total  result__total--final">fail</td>
+      <td class="result__total  result__total--final">${stats.result2.final}</td>
     </tr>
-  </table>
-  <table class="result__table">
+  </table>`;
+
+const result3 = `<table class="result__table">
     <tr>
-      <td class="result__number">3.</td>
+      <td class="result__number">${stats.result3.number}</td>
       <td colspan="2">
         <ul class="stats">
           <li class="stats__result stats__result--wrong"></li>
@@ -94,20 +139,27 @@ let statsTemplate = `<header class="header">
           <li class="stats__result stats__result--unknown"></li>
         </ul>
       </td>
-      <td class="result__points">×&nbsp;100</td>
-      <td class="result__total">900</td>
+      <td class="result__points">${stats.result3.points}</td>
+      <td class="result__total">${stats.result3.total}</td>
     </tr>
     <tr>
       <td></td>
-      <td class="result__extra">Бонус за жизни:</td>
-      <td class="result__extra">2&nbsp;<span class="stats__result stats__result--heart"></span></td>
-      <td class="result__points">×&nbsp;50</td>
-      <td class="result__total">100</td>
+      <td class="result__extra">${stats.result3.extra1.name}</td>
+      <td class="result__extra">${stats.result3.extra1.value}<span class="stats__result stats__result--heart"></span></td>
+      <td class="result__points">${stats.result3.extra1.points}</td>
+      <td class="result__total">${stats.result3.extra1.total}</td>
     </tr>
     <tr>
-      <td colspan="5" class="result__total  result__total--final">950</td>
+      <td colspan="5" class="result__total  result__total--final">${stats.result3.final}</td>
     </tr>
-  </table>
+  </table>`;
+
+const statsTemplate = `${header}
+<div class="result">
+  <h1>${stats.title}</h1>
+  ${result1}
+  ${result2}
+  ${result3}
 </div>`;
 
 const statsElement = getElementFromTemplate(statsTemplate);
